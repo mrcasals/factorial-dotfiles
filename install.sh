032b1c81f17c9abd143f1f19a1a687505fd2173c
Marc Riera <mrc2407@gmail.com>
2022-07-20 15:06:21 +0200

    Install dependencies if they're not there
    We do this so that the dotfiles can work as standalone for 
    any gitpod workspace for any other repo outside Factorial.

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Install chezmoi if it doesn't exist so that we can actually install dotfiles
+[[ ! -f ~/bin/chezmoi ]] && sh -c "$(curl -fsLS chezmoi.io/get)"
+
+# Install some deps if they're not there
+if ! command -v nvim &> /dev/null
+then
+  sudo apt-get update -qq && \
+  sudo apt-get install -o Dpkg::Options::=--force-confdef -yq --no-install-recommends \
+  diff-so-fancy \
+  neovim \
+  tmux \
+  tmuxinator && \
+  sudo apt-get install -y -o Dpkg::Options::="--force-overwrite" \
+  bat \
+  zstd \
+  ripgrep && \
+  sudo rm -rf /var/lib/apt/lists/*
+fi
+
+
 ~/bin/chezmoi init
 
 pushd ~/.local/share/chezmoi
